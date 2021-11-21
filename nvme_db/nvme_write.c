7d1b72c67cdb9d0e16f1540896691db034bdee00
Sophia Wisdom <sophiawisdom1999@gmail.com>
2021-11-21 01:09:04 -0800

    blah

--- nvme_db/nvme_write.c
+++ nvme_db/nvme_write.c
@@ -131,8 +131,6 @@ int nvme_append(struct state *state, int data_length, void *data) {
      *  0 on the namespace, and then later read it back into a separate buffer
      *  to demonstrate the full I/O path.
      */
-    int *int_buf = sequence.buf;
-    *int_buf = data_length;
     *((int *)sequence.buf) = data_length;
     memcpy(sequence.buf+4, data, data_length);
 
