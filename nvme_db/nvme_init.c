093a05de647360ae3c6197712b10dc4e4b790e6d
Sophia Wisdom <sophiawisdom1999@gmail.com>
2021-11-20 22:20:54 -0800

    blah

--- nvme_db/nvme_init.c
+++ nvme_db/nvme_init.c
@@ -13,6 +13,65 @@
 #include "spdk/nvme_zns.h"
 #include "spdk/env.h"
 
+static bool
+probe_cb(void *cb_ctx, const struct spdk_nvme_transport_id *trid,
+     struct spdk_nvme_ctrlr_opts *opts)
+{
+    printf("Attaching to %s\n", trid->traddr);
+
+    return true;
+}
+
+static void
+attach_cb(void *cb_ctx, const struct spdk_nvme_transport_id *trid,
+      struct spdk_nvme_ctrlr *ctrlr, const struct spdk_nvme_ctrlr_opts *opts)
+{
+    int nsid;
+    struct ctrlr_entry *entry;
+    struct spdk_nvme_ns *ns;
+    const struct spdk_nvme_ctrlr_data *cdata;
+
+    entry = malloc(sizeof(struct ctrlr_entry));
+    if (entry == NULL) {
+        perror("ctrlr_entry malloc");
+        exit(1);
+    }
+
+    printf("Attached to %s\n", trid->traddr);
+
+    /*
+     * spdk_nvme_ctrlr is the logical abstraction in SPDK for an NVMe
+     *  controller.  During initialization, the IDENTIFY data for the
+     *  controller is read using an NVMe admin command, and that data
+     *  can be retrieved using spdk_nvme_ctrlr_get_data() to get
+     *  detailed information on the controller.  Refer to the NVMe
+     *  specification for more details on IDENTIFY for NVMe controllers.
+     */
+    cdata = spdk_nvme_ctrlr_get_data(ctrlr);
+
+    snprintf(entry->name, sizeof(entry->name), "%-20.20s (%-20.20s)", cdata->mn, cdata->sn);
+
+    entry->ctrlr = ctrlr;
+    TAILQ_INSERT_TAIL(&g_controllers, entry, link);
+
+    /*
+     * Each controller has one or more namespaces.  An NVMe namespace is basically
+     *  equivalent to a SCSI LUN.  The controller's IDENTIFY data tells us how
+     *  many namespaces exist on the controller.  For Intel(R) P3X00 controllers,
+     *  it will just be one namespace.
+     *
+     * Note that in NVMe, namespace IDs start at 1, not 0.
+     */
+    for (nsid = spdk_nvme_ctrlr_get_first_active_ns(ctrlr); nsid != 0;
+         nsid = spdk_nvme_ctrlr_get_next_active_ns(ctrlr, nsid)) {
+        ns = spdk_nvme_ctrlr_get_ns(ctrlr, nsid);
+        if (ns == NULL) {
+            continue;
+        }
+        register_ns(ctrlr, ns);
+    }
+}
+
 int initialize(struct state *state) {
     TAILQ_INIT(&state -> g_namespaces);
     TAILQ_INIT(&state -> g_controllers);
@@ -53,11 +112,12 @@ int initialize(struct state *state) {
         return 1;
     }
 
-    if (TAILQ_EMPTY(&g_controllers)) {
+    if (TAILQ_EMPTY(&state -> g_controllers)) {
         fprintf(stderr, "no NVMe controllers found\n");
         cleanup();
         return 1;
     }
 
     printf("Initialization complete.\n");
+    return 0;
 }
