6d0dbc252820df5786a69efe53ff348cf2a983b7
Sophia <sophia.wisdom1999@gmail.com>
2021-11-27 21:22:12 -0600

    cb state

--- nvme_db/nvme_write_key_async.c
+++ nvme_db/nvme_write_key_async.c
@@ -22,7 +22,7 @@ void write_complete(void *arg, const struct spdk_nvme_cpl *completion) {
      * caller is aware that an error occurred.
      */
     if (spdk_nvme_cpl_is_error(completion)) {
-        spdk_nvme_qpair_print_completion(sequence->ns_entry->qpair, (struct spdk_nvme_cpl *)completion);
+        spdk_nvme_qpair_print_completion(cb_state->ns_entry->qpair, (struct spdk_nvme_cpl *)completion);
         fprintf(stderr, "I/O error status: %s\n", spdk_nvme_cpl_get_status_string(&completion->status));
         cb_state -> callback(cb_state -> cb_arg, WRITE_IO_ERROR);
         free(cb_state);
