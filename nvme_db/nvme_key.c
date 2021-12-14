9f9beabf96ac4683713355649742d411a94649a9
Sophia <sophia.wisdom1999@gmail.com>
2021-12-14 01:21:42 -0500

    blah

--- nvme_db/nvme_key.c
+++ nvme_db/nvme_key.c
@@ -124,6 +124,12 @@ static bool should_flush_writes(struct db_state *db) {
     return false;
 }
 
+static void print_key(struct db_state *db, struct ram_stored_key key) {
+            printf("Key has length %d, hash %d, vla offset %d, flags %d, data length %d data loc %llubn\n",
+        key.key_length, key.key_hash, key.key_offset, key.flags, key.data_length, key.data_loc);
+        printf("key itself is %s\n", db -> key_vla + key.key_offset); // todo print only till end of key
+}
+
 // PUBLIC API
 
 void *create_db() {
@@ -267,12 +273,6 @@ void poll_db(void *opaque) {
     while ((clock() - begin_clock) < 5000) { // poll for completions for up to 5ms
         spdk_nvme_qpair_process_completions(db->main_namespace->qpair, 0);
     }
-}
-
-static void print_key(struct db_state *db, struct ram_stored_key key) {
-            printf("Key has length %d, hash %d, vla offset %d, flags %d, data length %d data loc %llubn\n",
-        key.key_length, key.key_hash, key.key_offset, key.flags, key.data_length, key.data_loc);
-        printf("key itself is %s\n", db -> key_vla + key.key_offset); // todo print only till end of key
 }
 
 void print_keylist(struct db_state *db) {
