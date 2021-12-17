45c03a25f7a301712574e01b57e49be1aa069e89
Sophia <sophia.wisdom1999@gmail.com>
2021-12-17 02:16:45 -0500

    configurable timing

--- automated_interface.c
+++ automated_interface.c
@@ -108,9 +108,11 @@ int main(int argc, char **argv) {
         data -> key = key;
         data -> expected_value = value;
         write_value_async(db, key, value, write_callback, data);
-        for (int i = 0; i < (argc > 2 ? 100 : 0); i++) {
-            poll_db(db);
-            usleep(10000);
+        if (argc > 2) {
+            for (int i = 0; i < 100; i++) {
+                poll_db(db);
+                usleep(10000);
+            }
         }
     }
 
