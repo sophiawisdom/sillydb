f96acbc75585dc78044043c9f291367d8556c7bd
Sophia <sophia.wisdom1999@gmail.com>
2021-12-17 03:27:30 -0500

    zeroes

--- nvme_db/nvme_write_key_async.c
+++ nvme_db/nvme_write_key_async.c
@@ -165,7 +165,7 @@ void flush_writes(struct db_state *db) {
         printf("setting %d bytes from %lld to 'a'\n", write_size - buf_bytes_written, buf_bytes_written + (current_sector*db -> sector_size));
 #endif
         db -> current_sector_bytes = db -> sector_size - (write_size - buf_bytes_written);
-        memset(&flush_writes_cb_state -> buf[buf_bytes_written], 'a', write_size-buf_bytes_written);
+        memset(&flush_writes_cb_state -> buf[buf_bytes_written], 0, write_size-buf_bytes_written);
         memcpy(db -> current_sector_data, &flush_writes_cb_state -> buf[write_size - db -> sector_size], db -> current_sector_bytes);
     } else {
         db -> current_sector_bytes = 0;
