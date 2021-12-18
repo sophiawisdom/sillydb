24bf4292439444d9741682427fc57d35a36d4566
Sophia <sophia.wisdom1999@gmail.com>
2021-12-18 02:15:15 -0500

    blah

--- automated_interface.c
+++ automated_interface.c
@@ -135,6 +135,7 @@ int data_thread(struct data_generator *generator) {
     memset(random_state, 0, sizeof(random_state));
     initstate_r(data_seed,random_state,sizeof(random_state),&buffer);
     srandom_r(data_seed, &buffer);
+    printf("Buffer first is %.16s\n", &buffer);
 
     while (!generator -> reset) {
         _Atomic int *data = malloc(64*1024);
@@ -168,6 +169,7 @@ int data_thread(struct data_generator *generator) {
     initstate_r(data_seed,random_state,sizeof(random_state),&buffer);
     */
     srandom_r(data_seed, &buffer);
+    printf("Buffer second is %.16s\n", &buffer);
 
     while (1) {
         int *data = malloc(64*1024);
